a0aaced49ae6937ee82b839c9e0fd4a91f382154
James Wen <jrw2175@columbia.edu>
2014-01-19 01:20:26 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoundSystem::Application.config.secret_key_base = '656fcf8fc0ffb842a58fc998b3f4e6f585fa8607f492970ac03705cd49f38d86e8cd7e9099342bac00effee9c459254a29b21b4aa7940cf62b9e92e88620793b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SoundSystem::Application.config.session_store :cookie_store, key: '_SoundSystem_session'
