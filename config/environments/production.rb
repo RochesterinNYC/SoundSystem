abef234ba3f43c9443b654a268946128158564ca
James Wen <jrw2175@columbia.edu>
2014-02-17 19:14:47 -0500

    Implemented google analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ SoundSystem::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  GA.tracker = "UA-35915936-5"
 end
